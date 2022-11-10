2c270d08455528929e9d53a324e4e3fa76205647
Radium <2043869032@qq.com>
2022-11-11 00:28:22 +0800

    chore: commit

---
+++ packages/vue/edit-cell.ts
@@ -0,0 +1,5 @@
+export const editCellProps = {
+  modelValue: {
+    type: [Number, String],
+  },
+};
--- packages/vue/edit-cell.tsx
+++ packages/vue/edit-cell.tsx
@@ -1,6 +1,13 @@
-import {defineComponent} from 'vue';
+import { defineComponent } from 'vue';
+import { editCellProps } from './edit-cell';
 
-export  default defineComponent({
-    props:{},
-    setup(){}
+export default defineComponent({
+  props: editCellProps,
+  emits: ['update:modelValue'],
+
+  setup(props, { slots, attrs }) {
+    return () => {
+      return <div>{attrs}</div>;
+    };
+  },
 });
