d4101c08c4ccca31fa9bc5b78a70aa25c22061f0
2043869032@qq.com <2043869032@qq.com>
2022-12-30 10:14:05 +0800

    chore: add theme-chalk build

--- packages/theme-chalk/gulpfile.ts
+++ packages/theme-chalk/gulpfile.ts
@@ -1,10 +1,22 @@
 import path from 'path';
 import gulpSass from 'gulp-sass';
 import dartSass from 'sass';
-import { src } from 'gulp';
+import { dest, src } from 'gulp';
+import cleanCSS from 'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
+
+const distFolder = path.resolve(__dirname, 'dist');
 
 function buildThemeChalk() {
   const sass = gulpSass(dartSass);
 
-  return src(path.resolve(__dirname, 'src/*.scss')).pipe(sass.sync).pipe();
+  return src(path.resolve(__dirname, 'src/*.scss'))
+    .pipe(sass.sync())
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(cleanCSS())
+    .pipe(dest(distFolder));
 }
+
+buildThemeChalk();
+
+export default buildThemeChalk;
